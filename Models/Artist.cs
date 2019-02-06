942f8b2879907dbebf913ea5e9dc2060dd561569
Ebrahim Zare <ebrahimzare@users.noreply.github.com>
2019-02-06 11:42:59 -0500

    Add files via upload

---
+++ Models/Artist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+    }
+
+
+
+
+
+}
